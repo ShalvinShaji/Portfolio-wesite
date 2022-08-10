7b7fb10d50c1679d2ef2104ad68f23d57dd52f46
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-10 23:31:43 +0530

    footer links  animation added

--- Js/main.js
+++ Js/main.js
@@ -74,4 +74,4 @@ sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
-
+sr.reveal('.footer__link', {distance: '20px', delay: 50, interval: 100})
