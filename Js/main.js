daa2403bb07fa5c702d84b1c9a8193efa66c2ad2
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-14 23:08:11 +0530

    Website modified - theme changed

--- Js/main.js
+++ Js/main.js
@@ -52,6 +52,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
+sr.reveal('.home__status', {delay: 200})
 sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
