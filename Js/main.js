e74da7bd8f4805b3b654ab2f68e415230c4b6c72
Shalvin shaji <shalvinshaji283@gmail.com>
2022-09-04 12:29:22 +0530

    animations removed

--- Js/main.js
+++ Js/main.js
@@ -40,23 +40,23 @@ sr.reveal('.home__social-icons', {delay:700, interval: 200})
 
 /*SCROLL ABOUT*/
 
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+// sr.reveal('.about__img', {delay: 500})
+// sr.reveal('.about__subtitle', {delay: 300})
+// sr.reveal('.about__profession', {delay: 400})
+// sr.reveal('.about__text', {delay: 500})
+// sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.set', {interval: 200})
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+// sr.reveal('.portfolio__img', {interval: 200})
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
-sr.reveal('.footer__link', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.footer__line', {distance: '20px', delay: 80, interval: 100})
-sr.reveal('.footer__social', {distance: '20px', delay: 50, interval: 100})
+// sr.reveal('.contact__subtitle', {})
+// sr.reveal('.contact__text', {interval: 200})
+// sr.reveal('.contact__input', {delay: 400})
+// sr.reveal('.contact__button', {delay: 600})
+// sr.reveal('.footer__link', {distance: '20px', delay: 50, interval: 100})
+// sr.reveal('.footer__line', {distance: '20px', delay: 80, interval: 100})
+// sr.reveal('.footer__social', {distance: '20px', delay: 50, interval: 100})
